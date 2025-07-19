2831355b8889273b2c329eaeac16d682dd0a43df
m-abdullah-khan7 <muhammadabdullahkhanjt@gmail.com>
2025-07-19 21:27:48 +0500

    finished the code was easier then expected

--- mainfile.py
+++ mainfile.py
@@ -1,1 +1,11 @@
+
+
 filename = input("Enter the file name or file path: ")
+line_number = 1
+with open(filename , 'r') as file:
+    lines = file.readlines()
+    for line in lines:
+
+        if line.find('error') != -1 or line.find('issue') != -1:
+            print(line_number ,line)
+        line_number += 1    
